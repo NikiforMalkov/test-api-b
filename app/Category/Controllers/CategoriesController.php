22b15354e73ebab229ccb606257e664183fb3c34
Nikifor <nikiformalkov@gmail.com>
2022-11-15 14:42:59 +0600

    feat: added update route to categories controller

--- app/Category/Controllers/CategoriesController.php
+++ app/Category/Controllers/CategoriesController.php
@@ -3,7 +3,8 @@
 namespace App\Category\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Category\Services\CategoryService;
+use App\Category\Services\CategoryServiceInterface;
+use App\Category\Requests\UpdateCategoryRequest;
 
 /**
  * @OA\Info(
@@ -45,9 +46,57 @@ class CategoriesController extends Controller
      *      )
      * )
      */
-    public function all(CategoryService $categoryService)
+    public function all(CategoryServiceInterface $categoryService)
     {
         return $categoryService->all();
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/category/{id}",
+     *      operationId="updateCategory",
+     *      tags={"Category"},
+     *      summary="update category",
+     *      description="update category",
+     *
+     *   @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="integer"
+     *      )
+     *   ),
+     *
+     *   @OA\RequestBody(
+     *      required=true,
+     *      description="Pass user credentials",
+     *      @OA\JsonContent(
+     *         required={"name"},
+     *         @OA\Property(property="name", type="string", example="CategoryName"),
+     *         @OA\Property(property="parent_id", type="integer", example="1"),
+     *         @OA\Property(property="index", type="integer", example="1"),
+     *      ),
+     *   ),
+     *
+     *   @OA\Response(
+     *       response=200,
+     *       description="Successful operation",
+     *       @OA\JsonContent(ref="#/components/schemas/Category"),
+     *   ),
+     *   @OA\Response(
+     *       response=422,
+     *       description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *       response=404,
+     *       description="Not found"
+     *   )
+     * )
+     */
+    public function update(UpdateCategoryRequest $request, CategoryServiceInterface $service) {
+        $updateCategoryDto = $request->data();
+        return $service->update($updateCategoryDto);
     }
 
 }
