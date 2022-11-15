72f15b07259f6fe943b53e69f1f6a7b3deb98eb2
Nikifor <nikiformalkov@gmail.com>
2022-11-15 14:40:49 +0600

    feat: added update function to category service interface

--- app/Category/Services/CategoryServiceInterface.php
+++ app/Category/Services/CategoryServiceInterface.php
@@ -2,12 +2,16 @@
 
 namespace App\Category\Services;
 
-use App\Entities\CategoryTreeNode;
+use App\Category\Dto\UpdateCategoryRequestDto;
+use App\Category\Entities\CategoryTreeNode;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 
 interface CategoryServiceInterface {
 
     public function all();
+
+    public function update(UpdateCategoryRequestDto $category): Category;
 
     public function buildTree(Collection $categories): CategoryTreeNode;
 
